b631c5ffd4ba4ddce5684fc2f23217ad59c95710
rudy <rudykierbel@gmail.com>
2025-01-09 11:36:07 +0100

    cleanup

--- src/models/jina-embeddings.ts
+++ src/models/jina-embeddings.ts
@@ -1,7 +1,9 @@
 import {JinaEmbeddings} from "@langchain/community/embeddings/jina";
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 export const jinaEmbeddings = new JinaEmbeddings({
-    apiKey: 'jina_3f47a80afd5b42d9985ba7fca8bedfe9WHcMCRMvQ7lQ-FheqfABgp7LF2ae',
+    apiKey: process.env.JINA,
     model: "jina-embeddings-v3",
     dimensions: 1024,
     normalized: true
